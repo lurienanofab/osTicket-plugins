273bc93cc8263efd927aa216da89690d9a012654
LNF Developers <devs@lnf.umich.edu>
2021-08-04 21:31:21 +0000

    Finished implementing api commands.

--- api-lnf/class.lnfapi.php
+++ api-lnf/class.lnfapi.php
@@ -3,6 +3,7 @@
 require_once 'class.apiutility.php';
 include_once INCLUDE_DIR.'class.api.php';
 include_once INCLUDE_DIR.'class.ticket.php';
+define('TICKET_RESOURCE_TABLE',TABLE_PREFIX.'ticket_resource');
 
 class LnfApiController extends ApiController {
 
@@ -23,30 +24,46 @@ class LnfApiController extends ApiController {
         $this->response($this->status_code, $content);
     }
 
+    function isPost() {
+        return $_SERVER['REQUEST_METHOD'] == 'POST';
+    }
+
     function processAction() {
         //the default action (no resource_id supplied) returns all open tickets
 
         $api = $this->createApi();
         $sdate = $api->sdate ? $api->sdate : $api->defaultStartDate();
 
+        $code = null;
         $result = array();
 
         try {
-            switch ($this->action){
+            switch ($this->action) {
                 case "add-ticket":
-                    //$result = $this->addTicket();
-                    //$this->outputTickets($this->selectTickets(), "[action:$this->action] created ticket #{$result['ticket']->extid} in {$result['timeTaken']} seconds");
-                    throw new Exception('Not yet implemented.');
+                    if ($this->isPost()) {
+                        $api->addTicket(array(
+                            "email"         => ApiUtility::getval("email", "", $_POST),
+                            "name"          => ApiUtility::getval("name", "", $_POST),
+                            "subject"       => ApiUtility::getval("subject", "", $_POST),
+                            "message"       => ApiUtility::getval("body", "", $_POST),
+                            "pri"           => ApiUtility::getval("pri", "", $_POST),
+                            "resource_id"   => ApiUtility::getval("resource_id", "", $_POST),
+                            "source"        => ApiUtility::getval("source", "", $_POST),
+                        ));
+                        $api->sdate = $sdate;
+                        $result = $this->openTicketsResult($api);
+                    } else {
+                        $code = 405;
+                        throw new Exception("Method not supported: " . $_SERVER['REQUEST_METHOD']);
+                    }
                     break;
                 case "ticket-detail":
                     $ticket = $api->getTicketDetail();
                     $result = array('error' => false, 'message' => "action: $this->action, ticketID: $api->ticketID", 'detail' => $ticket);
                     break;
                 case "dept-membership":
-                    //$staff_id = $this->getval("staff_id", 0);
-                    //$html = LNF::getDeptMembership(array("staff_id"=>$staff_id));
-                    //echo $html;
-                    throw new Exception('Not yet implemented.');
+                    $data = $api->getDeptMembership();
+                    echo $data;
                     break;
                 case "select-tickets-by-email":
                     $api->search = "by-email";
@@ -61,45 +78,63 @@ class LnfApiController extends ApiController {
                     $result = array('error' => false, 'message' => $this->getMessage($api, array('resource_id' => $api->resource_id)), 'data' => $data);
                     break;
                 case "post-message":
-                    //if ($this->ticketID != 0){
-                    //    $message = $this->getval("message", "");
-                    //    $ticket = TicketPlugin::getTicket($this->ticketID);
-                    //    $ticket->postMessage($message);
-                    //    $detail = $this->selectTicketDetail();
-                    //    $this->outputTicketDetail(array("error"=>false, "message"=>"ok: $this->ticketID", "detail"=>$detail));
-                    //}
-                    //else
-                    //    $this->outputTicketDetail(array("error"=>true, "message"=>"Invalid parameter: ticketID"));
-                    throw new Exception('Not yet implemented.');
+                    if( $this->isPost() ) {
+                        if( ($ticketID = ApiUtility::getnum("ticketID", 0, $_POST)) != 0 ) {
+                            if( !$user = User::lookupByEmail(ApiUtility::getval("email", "", $_POST)) ) {
+                                $detail = "User not found";
+                            } else {
+                                $ticket = Ticket::lookupByNumber($ticketID);
+                                $ticket->postMessage(array(
+                                    "userId"    => $user->getId(),
+                                    "message"   => ApiUtility::getval("message", "", $_POST),
+                                    "source"    => ApiUtility::getval("source", "", $_POST),
+                                ));
+                                $detail = $api->getTicketDetails($ticket);
+                            }
+                            $result = array("error"=>false, "message"=>"ok: $ticketID", "detail"=>$detail);
+                        } else {
+                            $result = array("error"=>true, "message"=>"Invalid parameter: ticketID");
+                        }
+                    } else {
+                        $code = 405;
+                        throw new Exception("Method not supported: " . $_SERVER['REQUEST_METHOD']);
+                    }
                     break;
                 case "summary":
-                    //$resources = $this->getval("resources", "");
-                    //if ($resources)
-                    //    echo $this->outputSummary(array("error"=>false, "message"=>"ok: $resources", "summary"=>LNF::summary($resources)));//$this->outputSummar>                else
-                    //    $this->outputSummary(array("error"=>true, "message"=>"Invalid parameter: resources"));
-                    throw new Exception('Not yet implemented.');
+                    $resource_id = $api->resource_id;
+                    $summary = $api->getSummary();
+
+                    if( $resource_id ) {
+                        $result = array("error" => false, "message" => "ok: $resource_id", "summary" => $summary);
+                    } else {
+                        $result = array("error" => true, "message" => "Invalid parameter: resource_id");
+                    }
                     break;
                 case "get-ticket-counts":
-                    //$user = new Staff($this->getval("staff_userID", ""));
-                    //$staleCount = LNF::getStaleTicketCount($user);
-                    //$overdueCount = LNF::getOverdueTicketCount($user);
-                    //echo json_encode(array("staleTicketCount"=>$staleCount, "overdueTicketCount"=>$overdueCount));
-                    throw new Exception('Not yet implemented.');
+                    $staleCount = $api->getStaleTicketCount(0);
+                    $overdueCount = $api->getOverdueTicketCount();
+                    echo json_encode(array("staleTicketCount"=>$staleCount, "overdueTicketCount"=>$overdueCount));
                     break;
                 case "":
                 case "get-open-tickets":
-                    $api->search = "by-daterange";
-                    $api->status = "open";
                     $api->sdate = $sdate;
-                    $data = $api->getTickets();
-                    $result = array('error' => false, 'message' => $this->getMessage($api), 'data' => $data);
+                    $result = $this->openTicketsResult($api);
                     break;
                 default:
                     throw new Exception('Unsupported action: ' . $this->action);
             }
         } catch (Exception $ex) {
-            $result = $this->getErrorContent($ex->getMessage());
+            $result = $this->getErrorContent($ex->getMessage(), $code ?? 500);
         }
+
+        return $result;
+    }
+
+    function openTicketsResult($api) {
+        $api->search = "by-daterange";
+        $api->status = "open";
+        $data = $api->getTickets();
+        $result = array('error' => false, 'message' => $this->getMessage($api), 'data' => $data);
         return $result;
     }
 
@@ -116,6 +151,7 @@ class LnfApiController extends ApiController {
         $api->status = ApiUtility::getval("status", "");
         $api->sdate = ApiUtility::getval("sdate", "");
         $api->edate = ApiUtility::getval("edate", "");
+        $api->staff_id = ApiUtility::getnum("staff_id", 0);
         return $api;
     }
 
@@ -163,6 +199,7 @@ class LnfApi {
 
     var $ticket_data;
     var $thread_data;
+    var $staff_data;
     var $search;
     var $ticket_id;
     var $ticketID;
@@ -175,10 +212,12 @@ class LnfApi {
     var $status;
     var $sdate;
     var $edate;
+    var $staff_id;
 
     function __construct() {
         $this->ticket_data = new TicketData();
         $this->thread_data = new ThreadData();
+        $this->staff_data  = new StaffData();
     }
 
     function criteria() {
@@ -209,7 +248,7 @@ class LnfApi {
                 "priority_desc"     => $this->priority_desc,
                 "status"            => $this->status,
                 "sdate"             => $this->sdate,
-                "edate"             => $this->edate
+                "edate"             => $this->edate,
             );
         }
     }
@@ -297,21 +336,159 @@ class LnfApi {
         return $result;
     }
 
+    function addTicket($vars) {
+        $ticket = $this->ticket_data->insert(array(
+            "mid"       => "",
+            "email"     => $vars['email'],
+            "name"      => $vars['name'],
+            "subject"   => $vars['subject'],
+            "message"   => $vars['message'],
+            "header"    => "",
+            "pri"       => $vars['pri'],
+            "source"    => $vars['source'],
+        ));
+
+        return $ticket;
+    }
+
     function defaultStartDate(){
         //first day of previous month
         $today = getdate();
         $fom = $today['year'].'-'.$today['mon'].'-1';
         return date('Y-m-d', strtotime("$fom -1 month"));
     }
+
+    function getDeptMembership() {
+        $rows = $this->staff_data->select(array("staff_id" => $this->staff_id));
+
+        $membership = array();
+
+        if (count($rows) > 0){
+            $deptPrimary = $rows[0]; //there should only be one
+            $membership[] = array(
+                'dept_id' => $deptPrimary['id'],
+                'dept_name' => $deptPrimary['name'],
+                'from' => 'staff'
+            );
+        }
+
+        $rows = $this->staff_data->selectDeptAccess(array("staff_id" => $this->staff_id));
+
+        // Determine if staff_id has access to any additional departments besides the one they belong to
+        if ($rows != null){
+            foreach ($rows as $deptSecondary){
+                $membership[] = array(
+                'dept_id' => $deptSecondary['id'],
+                'dept_name' => $deptSecondary['name'],
+                'from' => 'staff_dept_access'
+                );
+            }
+        }
+
+        $result = '';
+        if ($this->staff_id > 0){
+            $br = '';
+            foreach ($membership as $dept){
+                if ($dept['from'] == 'staff'){
+                    $result .= '<span class="dept-name-staff" style="font-weight: normal;">'.$dept['dept_name'].'</span><div class="group-depts" style="display: none;">';
+                } else {
+                    $result .= $br.'<span class="dept-name-group" style="font-style: italic;">'.$dept['dept_name'].'</span>';
+                    $br = '<br />';
+                }
+            }
+            $result .= '</div>';
+        } else {
+            $result .= '<div style="font-style: italic; color: #808080;">No departments found.</div>';
+        }
+
+        return $result;
+    }
+
+    function getSummary() {
+        $result = $this->ticket_data->selectResources(array(
+            "resource_id"   => $this->resource_id
+        ));
+
+        return $result;
+    }
+
+    function getStaleTicketCount($numdays = 5) {
+        $result = $this->staff_data->selectStaleTickets(
+            array("staff_id" => $this->staff_id),
+            $numdays
+        );
+
+        return $result;
+    }
+
+    function getOverdueTicketCount() {
+        $result = $this->staff_data->selectOverdueTickets(array(
+        "staff_id" => $this->staff_id
+        ));
+
+        return $result;
+    }
+
+    function getTicketDetails($ticket) {
+        $staff = $ticket->getStaff();
+        $topic = $ticket->getTopic();
+        $messages  = array();
+        $responses = array();
+
+        $threadEntries = $ticket->getClientThread()->all();
+        foreach( $threadEntries as $entry ) {
+            if( $entry->getType() == 'M' ) {
+                $messages[] = array(
+                    "msg_id"        => $entry->getId(),
+                    "created"       => $entry->getCreateDate(),
+                    "message"       => (string)$entry->getMessage(),
+                    "source"        => $entry->getSource(),
+                    "ip_address"    => $entry->ip_address,
+                    "attachments"   => $entry->getNumAttachments(),
+                );
+            } else {
+                $responses[] = array(
+                    "response_id"   => $entry->getId(),
+                    "msg_id"        => "",
+                    "staff_id"      => $entry->getStaffId(),
+                    "staff_name"    => (string)$entry->getStaff(),
+                    "response"      => (string)$entry->getMessage(),
+                    "ip_address"    => $entry->ip_address,
+                    "created"       => $entry->getCreateDate(),
+                    "attachments"   => $entry->getNumAttachments(),
+                );
+            }
+        }
+
+        $result = array(
+            "info" => array(
+                "ticketID"          => $ticket->getNumber(),
+                "subject"           => $ticket->getSubject(),
+                "status"            => $ticket->getStatus()->getName(),
+                "priority"          => $ticket->getPriority()->getDesc(),
+                "dept_name"         => $ticket->getDeptName(),
+                "created"           => $ticket->getCreateDate(),
+                "name"              => $ticket->getName()->getFull(),
+                "email"             => (string)$ticket->getEmail(),
+                "phone"             => $ticket->getPhoneNumber(),
+                "source"            => $ticket->getSource(),
+                "assigned_name"     => ($staff) ? $staff->getName()  : "",
+                "assigned_email"    => ($staff) ? $staff->getEmail() : "",
+                "help_topic"        => ($topic) ? $topic->getName()  : "",
+                "last_response"     => $ticket->getLastResponseDate(),
+                "last_message"      => $ticket->getLastMessageDate(),
+                "ip_address"        => $ticket->getIP(),
+                "due_date"          => $ticket->getDueDate(),
+            ),
+            "messages"  => $messages,
+            "responses" => $responses,
+        );
+
+        return $result;
+    }
 }
 
 class TicketData extends ApiData {
-    function getTicket($ticketID){
-        $id = Ticket::getIdByNumber($ticketID);
-        $ticket = Ticket::lookup($id);
-        return $ticket;
-    }
-
     function select($criteria) {
         // the goal here is to return the same columns as the old lnfapi version
         $sql = "SELECT t.ticket_id, t.number AS 'ticketID', t.dept_id, d.name AS 'dept_name', IFNULL(tp.priority_id, 0) AS 'priority_id', t.topic_id, t.staff_id, ue.address AS 'email', u.name"
@@ -325,7 +502,7 @@ class TicketData extends ApiData {
             ." LEFT JOIN ".LIST_ITEM_TABLE." li ON li.id = tcd.resource_id"
             ." LEFT JOIN ".PRIORITY_TABLE." tp ON tp.priority_id = tcd.priority"
             ." LEFT JOIN ".USER_TABLE." u ON u.id = t.user_id"
-            ." LEFT JOIN ".USER_EMAIL_TABLE." ue ON ue.id = t.user_email_id AND u.id = ue.user_id"
+            ." LEFT JOIN ".USER_EMAIL_TABLE." ue ON ue.user_id = t.user_id"
             ." LEFT JOIN ".USER_CDATA_TABLE." ucd ON ucd.user_id = u.id"
             ." LEFT JOIN ".STAFF_TABLE." s ON s.staff_id = t.staff_id"
             ." LEFT JOIN ".DEPT_TABLE." d ON d.id = t.dept_id";
@@ -339,6 +516,27 @@ class TicketData extends ApiData {
         $this->fixResources($result);
 
         return $result;
+    }
+
+    function selectResources($criteria) {
+        $resourceId = $this->escape(ApiUtility::getnum("resource_id", 0, $criteria));
+
+        $sql = "SELECT tr.resource_id, tp.priority_urgency, tp.priority_desc, COUNT(*) AS 'ticket_count' FROM ".TICKET_TABLE." t"
+             ." INNER JOIN ".TICKET_RESOURCE_TABLE." tr ON tr.ticket_id = t.ticket_id"
+             ." INNER JOIN ".FORM_ENTRY_TABLE." fe ON fe.object_id = t.ticket_id AND fe.object_type = 'T'"
+             ." INNER JOIN ".FORM_ANSWER_TABLE." fa ON fa.entry_id = fe.id AND fa.value_id IS NOT NULL"
+             ." LEFT JOIN ".TICKET_PRIORITY_TABLE." tp ON tp.priority_id = fa.value_id"
+             ." LEFT JOIN ".TICKET_STATUS_TABLE." ts ON ts.id = t.status_id"
+             ." WHERE tr.resource_id IN ($resourceId) AND ts.state IN ('open')"
+             ." GROUP BY tr.resource_id, tp.priority_urgency, tp.priority_desc";
+
+        return $this->execQuery($sql);
+    }
+
+    function insert($vars) {
+        $errors = array();
+        $ticket = Ticket::create($vars, $errors, "api");
+        return array('ticket' => $ticket, 'errors' => $errors);
     }
 
     function fixResources(&$tickets) {
@@ -452,6 +650,65 @@ class ThreadData extends ApiData {
     function where($criteria) {
         $ticket_id = $this->escape(ApiUtility::getnum("ticket_id", 0, $criteria));
         return " WHERE th.object_type = 'T' and th.object_id = $ticket_id";
+    }
+}
+
+class StaffData extends ApiData {
+    function select($criteria) {
+        $sql = "SELECT s.staff_id, d.id, d.name FROM ".STAFF_TABLE." s"
+            ." LEFT JOIN ".DEPT_TABLE." d ON d.id = s.dept_id";
+
+        $sql .= $this->where($criteria);
+
+        $result = $this->execQuery($sql);
+
+        return $result;
+    }
+
+    function selectDeptAccess($criteria) {
+        $sql = "SELECT s.staff_id, d.id, d.name FROM ".STAFF_TABLE." s"
+            ." LEFT JOIN ".STAFF_DEPT_TABLE." da ON da.staff_id = s.staff_id"
+            ." LEFT JOIN ".DEPT_TABLE." d ON d.id = da.dept_id";
+
+        $sql .= $this->where($criteria);
+
+        $result = $this->execQuery($sql);
+
+        return $result;
+    }
+
+    function selectStaleTickets($criteria, $numdays) {
+        $sql = "SELECT COUNT(*) AS 'stale_ticket_count' FROM ".TICKET_TABLE." t"
+             ." LEFT JOIN ".TICKET_STATUS_TABLE." ts ON ts.id = t.status_id"
+             ." LEFT JOIN ".THREAD_TABLE." th ON th.object_id = t.ticket_id";
+
+        $staff_id = $this->escape(ApiUtility::getnum("staff_id", 0, $criteria));
+
+        $sql .= " WHERE t.staff_id = $staff_id";
+        $sql .= " AND ts.state = 'open' AND DATEDIFF(NOW(), th.lastresponse) > ".$numdays;
+
+        $result = $this->execQuery($sql);
+
+        return $result[0]["stale_ticket_count"];
+    }
+
+    function selectOverdueTickets($criteria) {
+        $sql = "SELECT COUNT(*) AS 'overdue_ticket_count' FROM ".TICKET_TABLE." t"
+             ." LEFT JOIN ".TICKET_STATUS_TABLE." ts ON ts.id = t.status_id";
+
+        $staff_id = $this->escape(ApiUtility::getnum("staff_id", 0, $criteria));
+
+        $sql .= " WHERE t.staff_id = $staff_id";
+        $sql .= " AND ts.state = 'open' AND t.isoverdue = 1";
+
+        $result = $this->execQuery($sql);
+
+        return $result[0]["overdue_ticket_count"];
+    }
+
+    function where($criteria) {
+        $staff_id = $this->escape(ApiUtility::getnum("staff_id", 0, $criteria));
+        return " WHERE s.staff_id = $staff_id";
     }
 }
 
